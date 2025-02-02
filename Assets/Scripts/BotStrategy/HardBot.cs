da0f8eb5320749c2f8163becebee437de762503d
Ahmet Hakan Hafif <59022934+colegion@users.noreply.github.com>
2025-02-02 16:17:43 +0300

    Iteration
    Better prototype for ahrd bot

--- Assets/Scripts/BotStrategy/HardBot.cs
+++ Assets/Scripts/BotStrategy/HardBot.cs
@@ -9,7 +9,7 @@ namespace BotStrategy
     {
         private Bot _bot;
         private HashSet<CardValue> _playedCards = new HashSet<CardValue>();
-        private List<CardConfig> _seenCards = new List<CardConfig>();
+        private HashSet<CardValue> _seenCards = new HashSet<CardValue>();
 
         public void InjectBot(Bot bot)
         {
@@ -21,49 +21,38 @@ namespace BotStrategy
         {
             var cardsOnTable = GameController.Instance.GetCardsOnTable();
             
-            var newSeenCards = new List<CardConfig>();
-
             foreach (var card in cardsOnTable)
             {
-                var tempConfig = card.GetConfig();
-                
-                if (!_seenCards.Contains(tempConfig) && !_playedCards.Contains(tempConfig.cardValue))
-                {
-                    newSeenCards.Add(tempConfig);
-                }
+                _seenCards.Add(card.GetConfig().cardValue);
             }
-            
-            _seenCards.AddRange(newSeenCards);
 
             var hand = _bot.GetHand();
             Card lastCard = cardsOnTable.Count > 0 ? cardsOnTable[^1] : null;
 
             if (lastCard == null)
             {
-                var card = GetLeastProbableCard(hand);
-                _playedCards.Add(card.GetConfig().cardValue);
-                _bot.OnCardPlayed(card);
+                PlayLeastProbableCard(hand);
                 return;
             }
-            
+
             foreach (var card in hand)
             {
                 if (card.GetConfig().cardValue == lastCard.GetConfig().cardValue)
                 {
-                    _playedCards.Add(card.GetConfig().cardValue);
-                    _bot.OnCardPlayed(card);
+                    PlayCard(card);
                     return;
                 }
             }
 
             if (TryPlayJack()) return;
-            
-            var leastProbableCard = GetLeastProbableCard(hand);
-            if (leastProbableCard != null)
-            {
-                _playedCards.Add(leastProbableCard.GetConfig().cardValue);
-                _bot.OnCardPlayed(leastProbableCard);
-            }
+
+            PlayLeastProbableCard(hand);
+        }
+
+        private void PlayCard(Card card)
+        {
+            _playedCards.Add(card.GetConfig().cardValue);
+            _bot.OnCardPlayed(card);
         }
 
         private bool TryPlayJack()
@@ -72,38 +61,25 @@ namespace BotStrategy
             {
                 if (card.IsJackCard())
                 {
-                    _playedCards.Add(card.GetConfig().cardValue);
-                    _bot.OnCardPlayed(card);
+                    PlayCard(card);
                     return true;
                 }
             }
             return false;
         }
 
-        private Card GetLeastProbableCard(List<Card> hand)
+        private void PlayLeastProbableCard(List<Card> hand)
         {
             Dictionary<CardValue, int> cardFrequencies = new Dictionary<CardValue, int>();
-            
-            foreach (var value in _playedCards)
-            {
-                cardFrequencies.TryAdd(value, 1);
-                cardFrequencies[value]++;
-            }
-
-            foreach (var cardConfig in _seenCards)
-            {
-                var cardValue = cardConfig.cardValue;
-                cardFrequencies.TryAdd(cardValue, 1);
-                cardFrequencies[cardValue]++;
-            }
+            foreach (var value in _playedCards) cardFrequencies[value] = cardFrequencies.GetValueOrDefault(value, 0) + 1;
+            foreach (var value in _seenCards) cardFrequencies[value] = cardFrequencies.GetValueOrDefault(value, 0) + 1;
 
             Card leastProbableCard = null;
             int minFrequency = int.MaxValue;
+
             foreach (var card in hand)
             {
-                var cardValue = card.GetConfig().cardValue;
-                int frequency = cardFrequencies.GetValueOrDefault(cardValue, 0);
-
+                int frequency = cardFrequencies.GetValueOrDefault(card.GetConfig().cardValue, 0);
                 if (frequency < minFrequency)
                 {
                     minFrequency = frequency;
@@ -111,7 +87,10 @@ namespace BotStrategy
                 }
             }
 
-            return leastProbableCard;
+            if (leastProbableCard != null)
+            {
+                PlayCard(leastProbableCard);
+            }
         }
     }
 }
